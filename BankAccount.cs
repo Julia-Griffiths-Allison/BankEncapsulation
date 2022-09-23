3e98aad84eb97820ee6538d7020e1433d2cf9245
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-23 14:58:26 -0400

    first commit, finished the assignment in full

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public double Deposit(int d)
+        {
+            balance = balance + d;
+            return balance;
+        }
+
+        public double getBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using BankEncapsulation;
+
+BankAccount newPerson = new BankAccount();
+newPerson.Deposit(42);
+Console.WriteLine(newPerson.getBalance());
